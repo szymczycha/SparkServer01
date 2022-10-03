9407eb2cab077728c5e94d558617e19f70e347e1
szymczycha <szymczycha266@gmail.com>
2022-10-03 09:51:44 +0200

    ale latwo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,8 +14,67 @@ public class App {
         ArrayList<Car> cars = new ArrayList<>();
         externalStaticFileLocation("C:\\Users\\4pa\\Desktop\\SparkServer01\\src\\main\\resources\\public");
         get("/add", (req, res) -> addCar(req, res, cars));
+        get("/delete/:id", (req, res) -> deleteCar(req, res, cars));
+        get("/update/:id", (req, res) -> updateCar(req, res, cars));;
+        get("/deleteAll", (req, res) -> deleteAll(req, res, cars));
         get("/text", (req, res) -> textCar(req, res, cars));
         get("/json", (req, res) -> jsonCar(req, res, cars));
+        get("/html", (req, res) -> htmlCar(req, res, cars));
+    }
+
+    private static String htmlCar(Request req, Response res, ArrayList<Car> cars) {
+        String out = "";
+        out+="<style>table, td{border: 1px solid black;}</style>" +
+                "<table>\n" +
+                "    <tbody>\n" +
+                "    \n";
+        for(Car car : cars){
+            out+="<tr>" +
+                    "<td>" +
+                    car.getId() +
+                    "</td>" +
+                    "<td>" +
+                    car.getModel() +
+                    "</td>" +
+                    "<td>" +
+                    car.isDamaged() +
+                    "</td>" +
+                    "<td>" +
+                    car.getDoors() +
+                    "</td>" +
+                    "<td>" +
+                    car.getCountry() +
+                    "</td>" +
+                    "</tr>";
+        }
+        out+="</tbody>\n" +
+                "</table>";
+        return out;
+    }
+
+    private static String deleteAll(Request req, Response res, ArrayList<Car> cars) {
+        for(int i = cars.size()-1; i>=0; i--){
+            cars.remove(cars.get(i));
+        }
+        return "Cars deleted";
+    }
+
+    private static String updateCar(Request req, Response res, ArrayList<Car> cars) {
+        for(Car car : cars){
+            if(car.getId()==Integer.parseInt(req.params("id"))){
+                car.setDamaged();
+            }
+        }
+        return "Car updated";
+    }
+
+    private static String deleteCar(Request req, Response res, ArrayList<Car> cars) {
+        for(Car car : cars){
+            if(car.getId()==Integer.parseInt(req.params("id"))){
+                cars.remove(car);
+            }
+        }
+        return "Car deleted";
     }
 
     private static String textCar(Request req, Response res, ArrayList<Car> cars) {
@@ -27,6 +86,7 @@ public class App {
         res.type("application/json");
         return gson.toJson(cars, listType);
     }
+
     private static String addCar(Request req, Response res, ArrayList<Car> cars) {
         cars.add(new Car(cars.size()+1,req.queryParams("model"), Integer.parseInt(req.queryParams("doors")), Boolean.parseBoolean(req.queryParams("damaged")), req.queryParams("country")));
         System.out.println(cars);
@@ -38,9 +98,35 @@ public class App {
 
 class Car{
     private int id;
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isDamaged() {
+        return damaged;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     private String model;
     private int doors;
     private boolean damaged;
+
+    public void setDamaged() {
+        damaged = !damaged;
+    }
+
     private String country;
 
     public Car(int id, String model, int doors, boolean damaged, String country) {
